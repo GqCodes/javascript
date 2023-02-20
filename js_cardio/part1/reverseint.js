5aa2a4ce677ade73103bd2270ddb37aa3c8ea4eb
Chris Vargas <gqcodes@gmail.com>
2023-02-20 08:49:23 -0500

    js cardio: reverseint

---
+++ js_cardio/part1/reverseint.js
@@ -0,0 +1,13 @@
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {
+  const revString = int.toString().split('').reverse().join('');
+
+  return parseInt(revString) * Math.sign(int);
+}
+
+// Call Function
+const output = reverseInt(-12345);
+console.log(output);
