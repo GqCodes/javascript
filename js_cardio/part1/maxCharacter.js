64a3d7eec4976b2750dcc1b144cc583da8d5df64
Chris Vargas <gqcodes@gmail.com>
2023-02-22 09:21:14 -0500

    js cardio: maxchar

---
+++ js_cardio/part1/maxCharacter.js
@@ -0,0 +1,29 @@
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+
+  str.split('').forEach((char) => {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+// Call Function
+const output = maxCharacter('javascript');
+console.log(output);
