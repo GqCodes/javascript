8d69572e569a14594a0e0949ec86de2747b860a1
Chris Vargas <gqcodes@gmail.com>
2023-03-27 09:12:33 -0400

    add js cardio

---
+++ js_cardio/part2/longestWord.js
@@ -0,0 +1,13 @@
+// CHALLENGE 1: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+function longestWord(sen) {
+  // SOLUTION 1 - Return a single longest
+  // SOLUTION 2 - Return an array and include multiple words if they have the same length
+  // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+}
+
+// Call Function
+const output = longestWord('Hello, my name is Brad');
+console.log(output);
